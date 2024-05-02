20619c21f7af55966e8ceb8a8047f112075f2969
Jon Samp <sampjon@gmail.com>
2024-05-01 22:42:28 -0500

    adds 4

--- app/settings.tsx
+++ app/settings.tsx
@@ -89,7 +89,7 @@ export default function Settings() {
           <RNView style={{ top: -1 }}>
             <SettingsCogIcon />
           </RNView>
-          <Text style={[type.titleTwo, { marginLeft: 4 }]}>Settings 123</Text>
+          <Text style={[type.titleTwo, { marginLeft: 4 }]}>Settings 1234</Text>
         </RNView>
         <TouchableOpacity onPress={router.back}>
           <Text style={type.title}>Done</Text>
