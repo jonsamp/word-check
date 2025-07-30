31be39a4ef285c80da48cb01f0b4ec5c146d0039
Jon Samp <sampjon@gmail.com>
2025-07-30 14:35:38 -0500

    fix deploy to production,  upgrades to 53

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { ExpoConfig } from "expo/config";
 const isProduction = process.env.EAS_BUILD_PROFILE === "production";
 
 const config: ExpoConfig = {
-  version: "2024.52.4",
+  version: "2024.53.0",
   name: "Word Check",
   slug: "word-check",
   scheme: "wordcheck",
